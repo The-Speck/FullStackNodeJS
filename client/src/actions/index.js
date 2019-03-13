4a144bf5fba8c153c789d18417039b5b16b043d9
Alex <a.plyushchay@gmail.com>
2019-03-13 11:47:41 -0400

    Begin landing page set up

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import {
+    FETCH_USER
+} from './types';
 
-const fecthUser = () => {
-    axios.get('/api/user');
-}
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/user');
+    console.log('payload', res);
+    dispatch({
+        type: FETCH_USER,
+        payload: res.data
+    });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,34 @@
 const passport = require("passport");
 
 module.exports = (app) => {
-    app.get('/auth/google',
+    app.get(
+        '/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
         })
     )
 
-    app.get('/auth/google/callback',
-        passport.authenticate('google')
-    )
-
-    app.get('/api/logout', (req, res) => {
-            req.logout();
-            res.send(req.user);
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
         }
     )
 
-    app.get('/api/user', (req, res) => {
+    app.get(
+        '/api/logout', 
+        (req, res) => {
+            console.log('logging out user', req.user);
+            req.logout();
+            res.redirect('/');
+        },
+    )
+
+    app.get(
+        '/api/user', 
+        (req, res) => {
+            console.log('Sending user', req.user);
             res.send(req.user);
         }
     )
