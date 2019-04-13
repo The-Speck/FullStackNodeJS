63b543fcf711074abb1cc7ab93c120bec701fc36
Alex <a.plyushchay@gmail.com>
2019-04-12 22:24:01 -0400

    create env variables

--- client/src/index.js
+++ client/src/index.js
@@ -21,3 +21,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 );
+
+console.log('Strip key is: ', process.env.REACT_APP_STRIPE_KEY);
+console.log('Env is: ', process.env.NODE_ENV);
