30c27be8dabb8456adda56fada2a9b8caaa9183c
Alex <a.plyushchay@gmail.com>
2019-04-17 17:56:48 -0400

    finish mailer begin styling

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,55 +1,30 @@
-const sendgrid = require('sendgrid');
+const sgMail = require('@sendgrid/mail');
 const keys = require('../config/keys');
-const helper = sendgrid.mail;
 
-class Mailer extends helper.Mail {
+sgMail.setApiKey(keys.sendGridKey);
+
+class Mailer {
   constructor({ subject, recipients }, content) {
-    super();
-
-    this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.from = 'no-reply@emaily.com';
     this.subject = subject;
-    this.body = new helper.Content('text/html', content);
-    this.recipients = this.formatAddresses(recipients);
-
-    this.addContent(this.body);
-    this.addClickTracking();
-    this.addRecipients();
+    this.content = content;
+    this.recipients = recipients;
   }
 
-  formatAddresses(recipients) {
-    return recipients.map(({ email }) => {
-      return new helper.Email(email);
-    });
-  }
+  send() {
+    const request = {
+      to: this.recipients,
+      from: this.from,
+      subject: this.subject,
+      content: [
+        {
+          type: 'text/html',
+          value: this.content
+        }
+      ]
+    };
 
-  addClickTracking() {
-    const trackingSettings = new helper.TrackingSettings();
-    const clickTracking = new helper.ClickTracking(true, true);
-
-    trackingSettings.setClickTracking(clickTracking);
-    this.addTrackingSettings(trackingSettings);
-  }
-
-  addRecipients() {
-    const personalize = new helper.Personalization();
-
-    this.recipients.forEach(recipient => {
-      personalize.addTo(recipient);
-    });
-
-    this.addPersonalization(personalize);
-  }
-
-  async send() {
-    const request = this.sgApi.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: this.toJSON()
-    });
-
-    const response = this.sgApi.API(request);
-    return response;
+    return sgMail.send(request);
   }
 }
 
