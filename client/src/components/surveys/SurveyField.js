8e50f9013ae7df9aab3e3a36d00b3b344fcc599b
Alex <a.plyushchay@gmail.com>
2019-04-23 21:31:02 -0400

    begin validation

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 export default ({ input, label }) => {
-  console.log(input);
-
   return (
     <div>
       <label>{label}</label>
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+  {
+    label: 'Survey Title',
+    name: 'title'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject'
+  },
+  {
+    label: 'Email body',
+    name: 'body'
+  },
+  {
+    label: 'Recipient List',
+    name: 'emails'
+  }
+];
 
 class SurveyForm extends React.Component {
   renderFields() {
-    return (
-      <div>
-        <Field
-          label="Survey Title"
-          name="title"
-          type="text"
-          component={SurveyField}
-        />
-      </div>
-    );
+    return _.map(FIELDS, ({ label, name }) => (
+      <Field
+        key={name}
+        component={SurveyField}
+        type="text"
+        label={label}
+        name={name}
+      />
+    ));
   }
 
   render() {
@@ -21,13 +41,30 @@ class SurveyForm extends React.Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'You must provide a title';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
